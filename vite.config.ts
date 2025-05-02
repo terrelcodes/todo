2b4aa8e196f6fc1544353352ceb0d9b722ae57e8
Terrel Shumway <gh@terrel.codes>
2025-05-02 15:35:39 -0600

    load .env to get VITE_BASE_URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.VITE_BASE_URL,
+    define: {
+      __APP_ENV__: JSON.stringify(env),
+    },
+    plugins: [solidPlugin()],
+    server: {
+      port: 3000,
+    },
+    build: {
+      target: 'esnext',
+    },
+  }
 });
