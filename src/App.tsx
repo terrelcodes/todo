d500e883fa3330d4e930e3e9f48522653333fb9c
Terrel Shumway <gh@terrel.codes>
2025-05-02 15:09:53 -0600

    move static part to index.html change absolute references to relative

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import type { Component } from 'solid-js';
 import Counter from './Counter';
 
 const App: Component = () => {
+  
   return (
-    <>
-      <h1>Hello, Dave</h1>
-      <Counter label="eggs" start={12}/>
-      <p>Welcome to <a href="https://www.solidjs.com/">SolidJS</a>.</p>
-    </>
+    <Counter label="eggs" start={12}/>
   );
 };
 
